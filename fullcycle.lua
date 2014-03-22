95e8a60e8f62c2ae17c42fa4845bfffee67883d4
git <git@git.git>
2014-03-22 19:11:31 +0100

    Allow any seed, not just inside of rng modulus

--- fullcycle.lua
+++ fullcycle.lua
@@ -2,7 +2,8 @@ local bit = rawget(_G,"bit32") or bit
 return function(power, seed)
     power = power or 31
     local mask = bit.lshift(1, power)-1
-    seed = seed or math.random(mask)
+    seed = bit.band(seed or math.random(mask), mask)
+    local state = seed
     return (function(mask, state)
         if not seed then return end
         state = bit.band((state*1664525 + 1013904223), mask)
